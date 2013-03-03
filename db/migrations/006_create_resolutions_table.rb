138371d8f0cc0ee7e57d70db244e4437e580f13d
Paul <pnblackwell@gmail.com>
2013-03-02 17:22:18 -0700

    payload update

---
+++ db/migrations/006_create_resolutions_table.rb
@@ -0,0 +1,8 @@
+Sequel.migrations do
+  change do
+    create_table(:resolutions) do
+      primary_key :id
+      String      :resolution
+    end
+  end
+end
--- lib/traffic_spy/models/payload.rb
+++ lib/traffic_spy/models/payload.rb
@@ -1,22 +1,51 @@
 require 'json'
 
-module Payload
-  
-  def initialize
-    @payload_db = Sequel.sqlite('./db/...')
+module TrafficSpy
+  class Payload
+    def initialize
+      @payload      = input[:payload]
+      @url          = input[:url]
+      @requested_at = input[:requested_at]
+      @responded_in = input[:responded_in]
+      @referred_by  = input[:referred_by]
+      @request_type = input[:request_type]
+      @parameters        = input[:parameters]
+      @event_name        = input[:event_name]
+      @user_agent        = input[:user_agent]
+      @resolution_width  = input[:resolution_width]
+      @resolution_height = input[:resolution_height]
+    end
+
+    def self.table
+      DB.from(:payloads)
+    end
+
+    def save(identifier, payload)
+      DB.from(:payload).insert(
+        :url 
+        :requested_at
+        :responded_in
+        :referred_by
+        :request_type
+        :paramters
+        :event_name
+        :user_agent
+        :resolution_width
+        :resolution_height 
+        )
+      end
+
+
+    post '/:identifier/data' do
+    #1) create an instance of payload that has all of the properties as an array
+      info = File.read('payload.json')
+      payload = JSON.parse(info)
+    #2) check to see if there is a payload return status that matches the one being created
+      if payload.empty?
+    #2a) if so, return error
+        return 400 Bad Request
+    #3) store payload objects in a database
+      else
+
   end
-
-  def self.database
-
-  post '/:identifier/data' do
-  #1) create an instance of payload that has all of the properties as an array
-    info = File.read('payload.json')
-    payload = JSON.parse(info)
-  #2) check to see if there is a payload return status that matches the one being created
-    if payload.empty?
-  #2a) if so, return error
-      return 400 Bad Request
-  #3) store payload objects in a database
-    else
-      
 end
